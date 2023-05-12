ff1129e0f29c594be82efbe9604f6e14db5d93b5
noearobles <roblesnoe1030@gmail.com>
2023-05-12 18:39:26 -0500

    setup gh-pages

--- src/Router.js
+++ src/Router.js
@@ -8,22 +8,24 @@ to the About component.
 Remember to export the component at the end
 */
 
-import Home from './components/Home'
-import About from './components/About'
-import Car from './components/Car'
-import React from 'react'
-import { Routes, Route } from 'react-router'
-import Error from './components/Error'
+import Home from "./components/Home";
+import About from "./components/About";
+import Car from "./components/Car";
+import React from "react";
+import { Routes, Route, HashRouter } from "react-router";
+import Error from "./components/Error";
 // Write component imports here //
 
 export default function Router() {
     return (
-        <Routes>
-            <Route path='/fake-car-dashboard' element={<Home />} />
-            <Route path='/fake-car-dashboard/about' element={<About />} />
-            <Route path='/fake-car-dashboard/car/:id' element={<Car />} />
-            <Route path='*' element={<Error />} />
-        </Routes>
-    )
+        <HashRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/car/:id" element={<Car />} />
+                <Route path="*" element={<Error />} />
+            </Routes>
+        </HashRouter>
+    );
 }
 // Start Router function here //
